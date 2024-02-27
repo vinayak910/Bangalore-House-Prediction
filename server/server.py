0b9421d8b601cad0f62893b484a1807e4cc273f8
Vinayak Chhabra <vinayakchhabra545.vc@gmail.com>
2024-02-27 16:57:00 +0530

    first commit

---
+++ server/server.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home(): 
+    return "<a href='./static/app.html'>GO TO APP </a>"
+
+
+@app.route('/api/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/api/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    util.get_location_names()
+    util.load_saved_artifacts()
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+
+    app.run(debug=True)
